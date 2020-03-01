998a3661b8a43d68b592aa6ffa3b1b9c6e3edfe4
SaucyPigeon <emilianb@hotmail.co.uk>
2020-03-01 11:29:40 +0000

    Added package references, improved formatting

---
+++ Source/Rainbeau's Wild Cultivation/Rainbeau's Wild Cultivation-RW1.0/RWC_Initializer.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using RimWorld;
+using Verse;
+
+namespace RWC_Code
+{
+	[StaticConstructorOnStartup]
+	internal static class RWC_Initializer
+	{
+		static RWC_Initializer()
+		{
+			HarmonyInstance harmony = HarmonyInstance.Create("net.rainbeau.rimworld.mod.wildcultivation");
+			harmony.Patch(AccessTools.Method(typeof(Designator_ZoneAdd_Growing), "CanDesignateCell"), new HarmonyMethod(typeof(Designator_ZoneAdd_Growing_RWC), "CanDesignateCellPrefix"), null);
+			harmony.Patch(AccessTools.Method(typeof(Designator_ZoneAdd_Growing), "CanDesignateCell"), null, new HarmonyMethod(typeof(Designator_ZoneAdd_Growing_RWC), "CanDesignateCellPostfix"));
+			LongEventHandler.QueueLongEvent(Setup, "LibraryStartup", false, null);
+		}
+
+		public static void Setup()
+		{
+			ThingDef.Named("PlantWildCotton").plant.harvestedThingDef = ThingDefOf.Plant_Cotton.plant.harvestedThingDef;
+			ThingDef.Named("PlantWildCotton").plant.harvestYield = ThingDefOf.Plant_Cotton.plant.harvestYield;
+
+			ThingDef.Named("PlantWildDevilstrand").plant.harvestedThingDef = ThingDefOf.Plant_Devilstrand.plant.harvestedThingDef;
+			ThingDef.Named("PlantWildDevilstrand").plant.harvestYield = ThingDefOf.Plant_Devilstrand.plant.harvestYield;
+		}
+	}
+    
+}
---
+++ Source/Rainbeau's Wild Cultivation/Rainbeau's Wild Cultivation-RW1.0/ThingDefOf.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RWC_Code
+{
+	[DefOf]
+	public static class ThingDefOf
+	{
+		public static ThingDef Plant_Cotton;
+		public static ThingDef Plant_Devilstrand;
+		public static ThingDef Plant_Potato;
+	}
+}
