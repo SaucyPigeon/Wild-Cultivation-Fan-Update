55fc029051feb2379b95d308795dc7177fb143cc
SaucyPigeon <emilianb@hotmail.co.uk>
2020-03-01 15:18:00 +0000

    Added 1.1 project, created multi-version folders.

---
+++ Source/Rainbeau's Wild Cultivation/Rainbeau's Wild Cultivation-RW1.1/Designator_ZoneAdd_Growing_RWC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace RWC_Code
+{
+	public static class Designator_ZoneAdd_Growing_RWC
+	{
+		public static bool CanDesignateCellPrefix(ref float __state)
+		{
+			__state = ThingDefOf.Plant_Potato.plant.fertilityMin;
+			ThingDefOf.Plant_Potato.plant.fertilityMin = 0.01f;
+			return true;
+		}
+
+		public static void CanDesignateCellPostfix(ref float __state)
+		{
+			ThingDefOf.Plant_Potato.plant.fertilityMin = __state;
+		}
+	}
+}
