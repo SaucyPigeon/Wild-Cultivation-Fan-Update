4def7b610bf998203d812a10059525ff1da72677
SaucyPigeon <emilianb@hotmail.co.uk>
2020-03-24 09:55:06 +0000

    Added Harmony requirement warning

--- Source/Rainbeau's Wild Cultivation/Rainbeau's Wild Cultivation-RW1.1/RWC_Initializer.cs
+++ Source/Rainbeau's Wild Cultivation/Rainbeau's Wild Cultivation-RW1.1/RWC_Initializer.cs
@@ -11,6 +11,10 @@ namespace RWC_Code
 		{
 			const string Id = "net.rainbeau.rimworld.mod.wildcultivation";
 			var harmony = new Harmony(Id);
+
+			// Will update to require Harmony as external mod soon
+			Log.Warning($"Wild Cultivation - Fan Update will be using Harmony as an external dependency when it next updates. To avoid wiping your mod list, please download and use Harmony now.");
+
 			harmony.Patch(AccessTools.Method(typeof(Designator_ZoneAdd_Growing), "CanDesignateCell"), new HarmonyMethod(typeof(Designator_ZoneAdd_Growing_RWC), "CanDesignateCellPrefix"), null);
 			harmony.Patch(AccessTools.Method(typeof(Designator_ZoneAdd_Growing), "CanDesignateCell"), null, new HarmonyMethod(typeof(Designator_ZoneAdd_Growing_RWC), "CanDesignateCellPostfix"));
 
